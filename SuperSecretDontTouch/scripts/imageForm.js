50cee5526983e363f684850bee6dd3a0afe9023d
Travis Kerr <kerrta@users.noreply.github.com>
2019-06-23 13:37:12 -0400

    Mockup for image upload interface

---
+++ SuperSecretDontTouch/scripts/imageForm.js
@@ -0,0 +1,7 @@
+function openForm() {
+  document.getElementById("imageForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("imageForm").style.display = "none";
+}
